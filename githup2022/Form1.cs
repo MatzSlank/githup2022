586f3216381a4ee8a3a5da0ca93116e8bcf4c307
MAX OCHOA <aochoa40@ucol.mx>
2022-10-09 23:08:28 -0500

    Agregar archivos de proyecto.

---
+++ githup2022/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace githup2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rojoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void azulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void acercaDeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Este programa fue creado por Max Ochoa.", "Acerca de . . .", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void BorrarColor()
+        {
+            rojoToolStripMenuItem.Checked = false;
+            azulToolStripMenuItem.Checked = false;
+        }
+        private void rojoToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            BorrarColor();
+            label1.ForeColor = Color.Red;
+            rojoToolStripMenuItem.Checked = true;
+        }
+
+        private void azulToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            BorrarColor();
+            label1.ForeColor = Color.Blue;
+            azulToolStripMenuItem.Checked = true;
+        }
+
+        private void comicSansToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comicSansToolStripMenuItem.Checked = true;
+            comicSansToolStripMenuItem.Checked = false;
+            label1.Font = new Font("Times New Roman", 14, label1.Font.Style);
+        }
+
+        private void timesNewRomanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comicSansToolStripMenuItem.Checked = true;
+            label1.Font = new Font("Comic Sans MS", 20, label1.Font.Style);
+            timesNewRomanToolStripMenuItem.Checked = false;
+        }
+    }
+}
